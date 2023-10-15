7a3b5b286e25e7e045bde4e41945542425747007
Faduma <aladirie@gmail.com>
2023-10-15 17:18:27 -0400

    added EJS and HTML

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs'); // This line sets EJS as view engine
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -15,15 +17,20 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-
 app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-  app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase }; // Pass the urlDatabase to the template
-    res.render("urls_index", templateVars); // Render the "urls_index.ejs" template
-  });
-  
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }; // Pass the urlDatabase to the template
+  res.render("urls_index", templateVars); // Render the "urls_index.ejs" template
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
