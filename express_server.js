492dc20a547368302398b4373a210dec130b306b
Faduma <aladirie@gmail.com>
2023-10-15 18:45:01 -0400

    added GET and POST

--- express_server.js
+++ express_server.js
@@ -2,6 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+//generateRandomString function goes here
+function generateRandomString(length) {
+  const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+  return result;
+}
+
+
+app.use(express.urlencoded({ extended: true }));
+
+// POST route to handle form submission
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); 
+});
+
 app.set('view engine', 'ejs'); // This line sets EJS as view engine
 
 const urlDatabase = {
@@ -24,6 +45,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; // Pass the urlDatabase to the template
   res.render("urls_index", templateVars); // Render the "urls_index.ejs" template
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
