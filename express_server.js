713937252185af617f05567a807d91252dfa2ab0
Faduma <aladirie@gmail.com>
2023-10-26 23:25:12 -0400

    Permission Features/longURL

--- express_server.js
+++ express_server.js
@@ -10,11 +10,17 @@ app.set('view engine', 'ejs');
 // Define the port number
 const PORT = 8080;
 
-// Sample database for storing URLs
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b6UTxQ": {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  "i3BoGr": {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
+
 // Define your `users` object to store registered users
 const users = {};
 
@@ -29,12 +35,12 @@ function generateRandomString(length) {
   return result;
 }
 //define finduser
-function findUserByEmail (email){
+function findUserByEmail(email) {
   for (const userId in users) {
     const user = users[userId];
     if (user.email === email) {
       return user;
-  
+
     }
   }
   return null;
@@ -60,19 +66,37 @@ app.get("/urls", (req, res) => {
 
 // Define the "/urls/new" route for creating a new URL
 app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    user: users[req.cookies.user_id], // Retrieve the user from cookies
-  };
-  res.render("urls_new", templateVars); // Render the "urls_new" template
+  const user = users[req.cookies.user_id]; // Retrieve the user from cookies
+
+  if (user) {
+    const templateVars = {
+      user,
+    };
+    res.render("urls_new", templateVars); // Render the "urls_new" template
+  } else {
+    res.redirect("/login"); // Redirect to the "/login" page if not logged in
+  }
 });
+
 
 // Define the POST route for submitting a new URL
 app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString(6); // Generate a short URL
-  urlDatabase[shortURL] = longURL; // Add the URL to the database
-  res.redirect(`/urls/${shortURL}`); // Redirect to the newly created URL
+  const user = users[req.cookies.user_id]; // Retrieve the user from cookies
+
+  if (user) {
+    const longURL = req.body.longURL;
+    const shortURL = generateRandomString(6); // Generate a short URL
+    urlDatabase[shortURL] = {
+      longURL,
+      userID: user.id, // Store the user ID along with the URL
+    };
+    res.redirect(`/urls/${shortURL}`); // Redirect to the newly created URL
+  } else {
+    // Respond with an HTML message explaining why they cannot create URLs
+    res.status(403).send("You must be logged in to create URLs.");
+  }
 });
+
 
 // Define the "/logout" route for logging out and clearing the "user_id" cookie
 app.post("/logout", (req, res) => {
@@ -102,16 +126,20 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL); // Redirect to the long URL
 });
 
-// Define the POST route for deleting a URL
+//delete route
 app.post("/urls/:id/delete", (req, res) => {
+  const user = users[req.cookies.user_id]; // Retrieve the user from cookies
   const shortURL = req.params.id;
-  if (urlDatabase[shortURL]) {
+
+  if (user && urlDatabase[shortURL] && urlDatabase[shortURL].userID === user.id) {
     delete urlDatabase[shortURL]; // Delete the URL from the database
     res.redirect("/urls"); // Redirect to the "/urls" page
   } else {
-    res.status(404).send("Short URL not found.");
+    res.status(404).send("URL not found.");
   }
 });
+
+
 
 // Define the POST route for editing a URL
 app.post("/urls/:id/edit", (req, res) => {
@@ -124,19 +152,19 @@ app.post("/urls/:id/edit", (req, res) => {
 // Define the POST route for handling user login
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  
-   // Look up the user by email in the users object
-   const user = findUserByEmail(email);
 
-       if (!user) {
-        return res.status(403).send("Email or user not found.");
-       }
-       if (bcrypt.compareSync(password, user.password)) {
-         res.cookie("user_id", user.id);
-         return res.redirect("/urls");
-       }
-   res.status(403).send("Email or password is incorrect.");
- });
+  // Look up the user by email in the users object
+  const user = findUserByEmail(email);
+
+  if (!user) {
+    return res.status(403).send("Email or user not found.");
+  }
+  if (bcrypt.compareSync(password, user.password)) {
+    res.cookie("user_id", user.id);
+    return res.redirect("/urls");
+  }
+  res.status(403).send("Email or password is incorrect.");
+});
 
 // Define a test route for displaying a greeting
 app.get("/hello", (req, res) => {
@@ -155,7 +183,7 @@ app.post("/register", (req, res) => {
     }
   }
 
-  users[userId] = { id: userId, email, password:bcrypt.hashSync(password, 10)};
+  users[userId] = { id: userId, email, password: bcrypt.hashSync(password, 10) };
   res.cookie("user_id", userId);
   res.redirect("/urls");
 });
@@ -175,6 +203,115 @@ app.get('/register', (req, res) => {
   };
   res.render('registration', templateVars);
 });
+
+// Create a GET Route for /login
+app.get("/login", (req, res) => {
+  // Check if the user is already logged in
+  if (req.cookies.user_id && users[req.cookies.user_id]) {
+    res.redirect("/urls"); // Redirect to /urls if logged in
+  } else {
+    const templateVars = {
+      user: users[req.cookies.user_id], // Retrieve the user from cookies
+    };
+    res.render("login", templateVars);
+  }
+});
+
+// Create a GET Route for /register
+app.get('/register', (req, res) => {
+  // Check if the user is already logged in
+  if (req.cookies.user_id && users[req.cookies.user_id]) {
+    res.redirect("/urls"); // Redirect to /urls if logged in
+  } else {
+    const templateVars = {
+      user: users[req.cookies.user_id],
+    };
+    res.render('registration', templateVars);
+  }
+});
+
+// Define the "/u/:id" route for redirecting to the long URL
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
+
+  if (longURL) {
+    res.redirect(longURL); // Redirect to the long URL if it exists
+  } else {
+    // Respond with a relevant HTML error message if the ID does not exist
+    res.status(404).send("<p>Short URL not found. Please check the URL and try again.</p>");
+  }
+});
+
+
+function urlsForUser(id) {
+  const userURLs = {};
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+  return userURLs;
+}
+
+
+app.get("/urls", (req, res) => {
+  const user = users[req.cookies.user_id];
+  if (user) {
+    const userURLs = urlsForUser(user.id);
+    const templateVars = {
+      user,
+      urls: userURLs,
+    };
+    res.render("urls_index", templateVars);
+  } else {
+    res.status(403).send("You must be logged in to view your URLs.");
+  }
+});
+
+app.get("/urls/:id", (req, res) => {
+  const user = users[req.cookies.user_id];
+  const shortURL = req.params.id;
+
+  if (user) {
+    if (urlDatabase[shortURL] && urlDatabase[shortURL].userID === user.id) {
+      const templateVars = {
+        user,
+        id: shortURL,
+        longURL: urlDatabase[shortURL].longURL,
+      };
+      res.render("urls_show", templateVars);
+    } else {
+      res.status(403).send("You do not have permission to view this URL.");
+    }
+  } else {
+    res.status(403).send("You must be logged in to view this URL.");
+  }
+});
+
+
+
+// Modify the "/urls" route
+app.get("/urls", (req, res) => {
+  const user = users[req.cookies.user_id]; // Retrieve the user from cookies
+  const userURLs = {};
+
+  for (const shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userID === user.id) {
+      userURLs[shortURL] = urlDatabase[shortURL];
+    }
+  }
+
+  const templateVars = {
+    user,
+    urls: userURLs, // Pass the user's URLs
+  };
+  res.render("urls_index", templateVars); // Render the "urls_index" template
+});
+
+
+
+
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
